47ff9cae3e39e3a4fb13564385ce7d0d879ec5d6
AnkushinDaniil <ankushin.daniil42@gmail.com>
2023-11-25 15:48:35 +0300

    first commit.  Connect button added

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+// import Header from "../components/Header"
+import ManualHeader from "../components/ManualHeader"
+// import LotteryEntrance from "../components/LotteryEntrance"
+// import { useMoralis } from "react-moralis"
+
+// const supportedChains = ["31337", "11155111"]
+
+export default function Home() {
+    // const { isWeb3Enabled, chainId } = useMoralis()
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Lottery App</title>
+                <meta
+                    name="description"
+                    content="Lottery smart contract frontend"
+                />
+            </Head>
+            <ManualHeader />
+        </div>
+    )
+}
